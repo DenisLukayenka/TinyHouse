0c90f4e8d3d02cce807389bcba3571df1b327c04
Dzianis <denis.lukayenka@gmail.com>
2019-12-07 05:47:30 +0300

    Init. Movable walls

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private List<GameObject> walls;
+    private HashSet<CoroutineObject> inactiveWalls;
+    private HashSet<CoroutineObject> activeWalls;
+    private RaycastManager raycastManager;
+
+    private float wallRelativeYUp = 4f;
+    private float wallSpeed = 8f;
+    
+    [SerializeField]
+    private Camera coreCamera;
+
+    void Start()
+    {
+        this.walls = GameObject.FindGameObjectsWithTag("Wall").ToList();
+        this.raycastManager = GameObject.Find("RaycastManager")?.GetComponent<RaycastManager>();
+        var castedWalls = this.raycastManager.CastRayToWalls(this.walls, this.coreCamera.gameObject).Distinct();
+
+        this.inactiveWalls = new HashSet<CoroutineObject>(castedWalls.Select(w => new CoroutineObject(){ GameObject = w }));
+
+        this.activeWalls = new HashSet<CoroutineObject>(this.walls.Where(w => !inactiveWalls.Any(iw => iw.GameObject == w))
+                                            .Select(x => new CoroutineObject{GameObject = x }).ToList());
+
+        Debug.Log("Inactive: " + string.Join(", ", inactiveWalls));
+        Debug.Log("activeWalls: " + string.Join(", ", activeWalls));
+
+        foreach(var wall in inactiveWalls)
+        {
+            this.StartCoroutine(this.UpWall(wall, wallRelativeYUp));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            var hittedWalls = this.raycastManager.CastRayToWalls(this.walls, this.coreCamera.gameObject).Distinct().ToList();
+            var activatedWalls = new List<CoroutineObject>();
+            Debug.Log("hittedWalls: " + string.Join(", ", hittedWalls));
+
+            foreach(var inactiveWall in this.inactiveWalls)
+            {
+                var target = hittedWalls.FirstOrDefault(hitted => hitted == inactiveWall.GameObject);
+
+                if(target is null)
+                {
+                    activatedWalls.Add(inactiveWall);
+                }
+            }
+
+            activatedWalls.ForEach(wall => 
+            {
+                if(wall.IsUpdatable)
+                {
+                    this.inactiveWalls.Remove(wall);
+                    this.StartCoroutine(this.DownWall(wall));
+                    this.activeWalls.Add(wall);
+                }
+            });
+
+            for(int i = 0; i < hittedWalls.Count; i++)
+            {
+                var target = this.activeWalls.FirstOrDefault(a => a.GameObject == hittedWalls[i]);
+
+                if(target != null && target.IsUpdatable)
+                {
+                    this.activeWalls.Remove(target);
+                    this.StartCoroutine(this.UpWall(target, this.wallRelativeYUp));
+                    this.inactiveWalls.Add(target);
+                }
+            }
+            Debug.Log("_______________________________________________________________");
+        }
+    }
+
+    IEnumerator UpWall(CoroutineObject coroutineObject, float newY) 
+    {   
+        var wall = coroutineObject.GameObject.transform.GetChild(0).gameObject;
+        coroutineObject.IsUpdatable = false;
+
+        while(wall.transform.localPosition.y <= newY)
+        {
+            Debug.Log($"UpWall {coroutineObject.IsUpdatable}");
+
+            var offSet = Vector3.up * Time.deltaTime * wallSpeed / wall.transform.position.y * 7f;
+            if(offSet.y + wall.transform.localPosition.y < newY)
+            {
+                wall.transform.localPosition += offSet;
+                yield return null;
+            }
+            else
+            {
+                coroutineObject.IsUpdatable = true;
+                Debug.Log($"UpWall {coroutineObject.IsUpdatable} + {wall.transform.name}");
+                wall.transform.localPosition = new Vector3(0, newY, 0);
+                yield break;
+            }
+        }
+    }
+
+    IEnumerator DownWall(CoroutineObject coroutineObject) 
+    {   
+        var wall = coroutineObject.GameObject.transform.GetChild(0).gameObject;
+        coroutineObject.IsUpdatable = false;
+
+        while(wall.transform.position.y >= wall.transform.parent.position.y)
+        {
+            var offSet = (Vector3.up * Time.deltaTime * wallSpeed * Mathf.Abs(wall.transform.localPosition.y));
+            Debug.Log($"DownWall {coroutineObject.IsUpdatable} + {wall.transform.name} + {offSet}");
+
+            if(offSet != Vector3.zero && offSet.y < wall.transform.localPosition.y)
+            {
+                wall.transform.localPosition -= offSet;
+                yield return null;
+            }
+            else
+            {
+                coroutineObject.IsUpdatable = true;
+                wall.transform.localPosition = Vector3.zero;
+                Debug.Log($"UpWall {coroutineObject.IsUpdatable}");
+                yield break;
+            }
+        }
+
+        
+
+    }
+
+    private class CoroutineObject
+    {
+        public GameObject GameObject{ get; set; }
+        public bool IsUpdatable { get; set; } = true;
+    }
+}
---
+++ Assets/Scripts/RaycastManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastManager : MonoBehaviour
+{
+    [SerializeField]
+    private Camera mainCamera;
+    private float cameraRotation;
+    private string maskName = "Walls";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*var oldMask = mainCamera.cullingMask;
+        mainCamera.cullingMask = (1 << LayerMask.NameToLayer("Walls"));*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown("e")){
+            StartCoroutine(RotateMe(Vector3.up * 90, 0.8f));
+        }
+    }
+
+    public IEnumerable<GameObject> CastRayToWalls(List<GameObject> targets, GameObject shooter)
+    {
+        LayerMask mask = LayerMask.GetMask("Walls");
+
+        foreach(var target in targets)
+        {
+            Vector3 diraction = target.transform.position - shooter.transform.position;
+            Debug.DrawRay(shooter.transform.position, diraction, Color.yellow);
+            
+            if(Physics.Raycast(shooter.transform.position, diraction, out RaycastHit hit, Mathf.Infinity, mask))
+            {
+                yield return hit.collider.gameObject;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Transform transform = Camera.main.transform;
+
+        /*if (Input.GetKeyUp("space"))
+        {
+            // Check for a Wall.
+            LayerMask mask = LayerMask.GetMask("Walls");
+            // Check if a Wall is hit.
+            if (Physics.Raycast(transform.position, transform.forward, 20.0f, mask))
+            {
+                Debug.Log("Fired and hit a wall");
+            }
+        }*/
+    }
+
+    IEnumerator RotateMe(Vector3 byAngles, float inTime) 
+     {   var fromAngle = transform.rotation;
+         var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
+         for(var t = 0f; t < 1; t += Time.deltaTime/inTime) {
+             transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+             yield return null;
+         }
+     }
+}
