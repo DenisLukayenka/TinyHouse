5584719c4b62b9f1cac9aafcf2a4d7d2a1345651
Dzianis <denis.lukayenka@gmail.com>
2019-12-10 23:27:09 +0300

    Implement drag items.

---
+++ Assets/Scripts/Inventory/ItemDragHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemDragHandler : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+	public void OnDrag(PointerEventData eventData)
+	{
+		this.transform.position = Input.mousePosition;
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		this.transform.localPosition = Vector3.zero;
+	}
+}
