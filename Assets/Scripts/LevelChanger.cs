273fad927dae95a52cfd7f3a1124a4d12ccbaa4e
Dzianis <denis.lukayenka@gmail.com>
2019-12-18 17:13:12 +0300

    Add main menu scene

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class LevelChanger : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _animator;
+
+    private GameObject prevLocation;
+    private GameObject newLocation;
+
+    public void FadeToLocation(GameObject prevLocation, GameObject newLocation)
+    {
+        this.prevLocation = prevLocation;
+        this.newLocation = newLocation;
+
+        _animator.SetTrigger("Fadeout");
+    }
+
+    public void OnFadeComplete()
+    {
+        Debug.Log("changed location");
+
+        prevLocation.SetActive(false);
+        newLocation.SetActive(true);
+
+        _animator.Play("Fade_in");
+    }
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void OnExitButtonPress()
+    {
+        Debug.Log("Quit game");
+        
+        Application.Quit();
+    }
+
+    public void OnNewGameButtonPress()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
